04205ac568cec35129091b5ead48fca7a1e11f47
Levis <quentin.levis.simplon@gmail.com>
2020-09-05 15:43:00 +0200

    add another glitch sprites and anim it

--- public/js/entities/glitch.js
+++ public/js/entities/glitch.js
@@ -1,37 +1,27 @@
 import Entity, {Sides} from '../Entity.js';
-import {loadSpriteSheet} from '../loader.js';
+import PendulumWalk from '../traits/PendulumWalk.js';
+import {loadSpriteSheet} from '../loaders.js';
 
-export function loadGlitch(){
+export function loadGlitch() {
     return loadSpriteSheet('glitch')
     .then(createGlitchFactory);
 }
 
 function createGlitchFactory(sprite) {
+    const walkAnim = sprite.animations.get('walk');
+
     function drawGlitch(context) {
-        sprite.draw('glitch', context, 0,0);
+        sprite.draw(walkAnim(this.lifetime), context, 0, 0);
     }
 
     return function createGlitch() {
         const glitch = new Entity();
-        glitch.size.set(16,16);
+        glitch.size.set(16, 16);
 
-        glitch.addTrait({
-            NAME: 'walk',
-            speed: 20,
-            obstruct(glitch, side) {
-                if (side === Sides.LEFT || side === Sides.RIGHT) {
-                    this.speed = -this.speed;
-                }
-            },
-            update(glitch) {
-                if (glitch.vel.x == 0 ) {
-                    this.speed = -this.speed
-                }
-                // console.log("velocity", glitch.vel.x)
-                glitch.vel.x = this.speed;
-            }
-        })
+        glitch.addTrait(new PendulumWalk());
+
         glitch.draw = drawGlitch;
+
         return glitch;
     };
 }
